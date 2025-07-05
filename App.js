87d49e2b876fc4448022280d5eef668b74b3bf8e
Hassan Ali <hasu77212.osman@gmail.com>
2025-07-05 10:49:37 +0500

    Update App.js

--- App.js
+++ App.js
@@ -5,9 +5,6 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SignupScreen from './screens/SignupScreen';
 import LoginScreen from './screens/LoginScreen';
 import HomeScreen from './screens/HomeScreen';
-import AddNoteScreen from './screens/AddNoteScreen';
-import NotesListScreen from './screens/NotesListScreen';
-
 
 const Stack = createNativeStackNavigator();
 
@@ -18,9 +15,6 @@ export default function App() {
         <Stack.Screen name="Signup" component={SignupScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="AddNote" component={AddNoteScreen} />
-<Stack.Screen name="NotesList" component={NotesListScreen} />
-
       </Stack.Navigator>
     </NavigationContainer>
   );
